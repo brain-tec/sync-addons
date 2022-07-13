75277e8b1ab3aadf58741a4916dd50114f4bdb54
Ivan Yelizariev <iel@odoo.com>
2022-07-13 19:37:23 +0200

    fixup: category menu is finally visible (but doesn't work - WIP)

--- multi_livechat/static/src/models/messaging_initializer/messaging_initializer.js
+++ multi_livechat/static/src/models/messaging_initializer/messaging_initializer.js
@@ -1,17 +1,19 @@
 /** @odoo-module **/
 
-import { registerInstancePatchModel } from "@mail/model/model_core";
 import { insertAndReplace } from "@mail/model/model_field_command";
+import { registerInstancePatchModel } from "@mail/model/model_core";
 
 registerInstancePatchModel(
     "mail.messaging_initializer",
-    "im_livechat/static/src/models/messaging_initializer/messaging_initializer.js",
+    "multi_livechat/static/src/models/messaging_initializer/messaging_initializer.js",
     {
         _initResUsersSettings(settings) {
             const data = {};
             _.each(this.messaging.discuss.getMLChatCategories(), (chat, field_name) => {
-                const NAME = field_name.split("_")[1];
-                const display_name = NAME.charAt(0).toUpperCase() + NAME.slice(1);
+                const NAME = field_name.split("categoryMLChat_")[1];
+                const display_name = (
+                    NAME.charAt(0).toUpperCase() + NAME.slice(1)
+                ).replace("_", " ");
                 const state_key = "is_discuss_sidebar_category_" + NAME + "_open";
                 data[field_name] = insertAndReplace({
                     isServerOpen: settings[state_key],
